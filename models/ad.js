3fc2aeafa4c616b2a7cf8995279ecc6ab02697f2
HarryKural <harrykural333@gmail.com>
2017-04-07 13:26:25 -0400

    added add & edit pages

--- models/ad.js
+++ models/ad.js
@@ -1,3 +1,24 @@
 /**
  * Created by Harshit Sharma on 07-Apr-2017.
  */
+// reference mongoose
+let mongoose = require('mongoose');
+
+// create ad schema
+let adSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: 'Title is required'
+    },
+    author: {
+        type: String,
+        required: 'Description is required'
+    },
+    price: {
+        type: Number,
+        min: 0.01
+    }
+});
+
+// make it public
+module.exports = mongoose.model('Ad', adSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+
+let Account = require('../models/account');
+let passport = require('passport');
+
+// auth check
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next(); // user is logged, so call the next function
+    }
+
+    res.redirect('/'); // not logged in so redirect to home
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', isLoggedIn, function(req, res, next) {
+
+    // use mongoose model to query mongodb for all users
+    Account.find(function(err, accounts) {
+        if (err) {
+            console.log(err);
+            res.end(err);
+            return;
+        }
+
+        // no error so send the users to the index view
+        res.render('users', {
+            title: 'Users List',
+            users: accounts,
+            user: req.user
+        });
+    });
 });
 
 module.exports = router;
